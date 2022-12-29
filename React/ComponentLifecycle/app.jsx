0b664371bc4e50486f7c3247bee3745babcd3019
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-29 18:57:08 +0000

    Completed Component Lifecycle Methods lesson from Codecademy

---
+++ React/ComponentLifecycle/app.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Introduction to Lifecycle Methods
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    )
+  }
+}
+
+// componentDidMount
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000)
+  }
+}
+
+// componentWillUnmount
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, oneSecond);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+}
+
+// componentDidUpdate
+export class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  render() {
+    return (
+      <div>
+        {this.props.isPrecise
+          ? this.state.date.toISOString()
+          : this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
+  startInterval() {
+    let delay;
+    if (this.props.isPrecise) {
+      delay = 100;
+    } else {
+      delay = 1000;
+    }
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, delay);
+  }
+  componentDidMount() {
+    this.startInterval();
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.isPrecise === prevProps.isPrecise) {
+      return;
+    }
+    clearInterval(this.intervalID);
+    this.startInterval();
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+}
