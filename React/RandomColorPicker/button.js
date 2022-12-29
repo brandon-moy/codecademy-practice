727d4e72f2897fbedf3f424de8f43e29fdcf8694
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-29 18:38:42 +0000

    Added walk through answer of bonus question of why is the text/button change light or dark

--- React/RandomColorPicker/button.js
+++ React/RandomColorPicker/button.js
@@ -11,3 +11,17 @@ export class Button extends React.Component {
     );
   }
 }
+
+// Bonus question: Why is the button light or dark based on the background?
+
+// The button calls the isLight function which reduces the number and checks if
+// the value is a "light" color
+// it evaluates and returns a boolean value which is then passed to the button
+// component through the light prop
+
+// in the Button component, it utilizes a ternary expression to determine if the
+// light value that was passed as a prop is true or false and then changes
+// the color of the button
+
+// this same logic is used when determinining whether the text on screen
+// should be white or black
