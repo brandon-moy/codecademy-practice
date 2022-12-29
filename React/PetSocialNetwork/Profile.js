37ad345760597d086abd048dff43ca303dd95a48
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-29 19:13:05 +0000

    Added conditional render is isLoading is false then we will display an image with the profile picture

--- React/PetSocialNetwork/Profile.js
+++ React/PetSocialNetwork/Profile.js
@@ -33,7 +33,9 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+          {!isLoading && <img src={this.state.userData.profilePictureUrl} alt="" />}
+        </div>
         <div className="profile-body">
           <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
