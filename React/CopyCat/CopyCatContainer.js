85034b774229a018cdea1e2999bc68f7b24eb617
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-04 18:57:55 +0000

    Modified CopyCatContainer to be rendered and renders CopyCat component

---
+++ React/CopyCat/CopyCatContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CopyCat } from './CopyCat'
+
+const images = {
+  copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
+  quietcat: 'https://content.codecademy.com/courses/React/react_photo_quietcat.png'
+};
+
+
+class CopyCatContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copying: true
+    };
+
+    this.toggleTape = this.toggleTape.bind(this);
+  }
+
+  toggleTape() {
+    this.setState({ copying: !this.state.copying })
+  }
+
+  render() {
+    const copying = this.state.copying;
+    const toggleTape = this.toggleTape
+
+    return (
+      <CopyCat />
+    );
+  };
+}
+
+
+ReactDOM.render(<CopyCatContainer />, document.getElementById('app'));
