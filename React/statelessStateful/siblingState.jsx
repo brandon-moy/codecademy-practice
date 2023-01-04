726d6b9cb78c56597afc68bd32fa3a756f5a6e76
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-04 17:26:14 +0000

    Completed Child Components update their Siblings' props lesson from Codecademy

---
+++ React/statelessStateful/siblingState.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child
+          onChange={this.changeName} />
+        <Sibling name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+class Sibling extends React.Component {
+  render() {
+    const name = this.props.name;
+    return (
+      <div>
+        <h1>Hey, my name is {name}!</h1>
+        <h2>Don't you think {name} is the prettiest name ever?</h2>
+        <h2>Sure am glad that my parents picked {name}!</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
